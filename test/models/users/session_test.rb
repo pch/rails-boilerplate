5853e50685cb0e86ec7fd1e344219d1219297326
Piotr Chmolowski <piotr@chmolowski.pl>
2022-01-01 14:10:41 +0100

    Session unit tests

--- test/models/users/session_test.rb
+++ test/models/users/session_test.rb
@@ -2,8 +2,41 @@ require "test_helper"
 
 module Users
   class SessionTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+    test "#create_new_session!" do
+      assert_difference "Users::Session.count" do
+        assert session.token.present?
+        assert session.accessed_at.present?
+      end
+    end
+
+    test "#authenticate" do
+      assert_equal session, Users::Session.authenticate(session.token)
+    end
+
+    test "#log_access" do
+      session.accessed_at = nil
+      session.log_access
+      assert session.accessed_at.present?
+    end
+
+    test "#revoke!" do
+      session.revoked_at = nil
+      session.revoke!
+      assert session.revoked_at.present?
+    end
+
+    test "#revoked?" do
+      session.revoked_at = nil
+      assert !session.revoked?
+
+      session.revoke!
+      assert session.revoked?
+    end
+
+    private
+
+    def session
+      @session ||= Users::Session.create_new_session!(User.system_user.reload)
+    end
   end
 end
