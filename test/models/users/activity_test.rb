81a57b326e43eb71480fd69501e869000a10de5a
Piotr Chmolowski <piotr@chmolowski.pl>
2022-01-01 13:52:56 +0100

    Activity test

--- test/models/users/activity_test.rb
+++ test/models/users/activity_test.rb
@@ -2,8 +2,10 @@ require "test_helper"
 
 module Users
   class ActivityTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+    test "geocode new records" do
+      activity = Users::Activity.new(user: User.system_user, action: "test", ip: "127.0.0.1")
+      Users::GeocodeActivityJob.expects(:perform_later).with(activity)
+      activity.save
+    end
   end
 end
