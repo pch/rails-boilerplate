8d5469a79c4e0935f1e72eed82651e379d048780
Piotr Chmolowski <piotr@chmolowski.pl>
2022-01-01 13:52:37 +0100

    Fix static pages controller tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,8 +1,13 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get static_pages_show_url
+  test "should get terms" do
+    get terms_url
+    assert_response :success
+  end
+
+  test "should get privacy policy" do
+    get privacy_policy_url
     assert_response :success
   end
 end
